ed2a32f86c7a234e728482f1869784d40f23a6ca
laurendobratz00 <laurendobratz@icloud.com>
2021-07-15 00:59:44 +0000

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int MaxInteger(List<int> myList)
+        {
+            if (myList.Count == 0)
+            {
+                Console.WriteLine("List is empty");
+                return -1;
+            }
+            int m = myList[0];
+            foreach (int value in myList)
+            {
+                if (value > m)
+                {
+                    m = value;
+                }
+            }
+            return m;
+        }
+    }
+

a234a3eb99b5bff1cc30adf88854bfcb43ff4696
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-07-27 16:10:56 -0500

    set health = value

--- 0x06-csharp-classes/2-enemy/2-enemy.cs
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -12,6 +12,7 @@ namespace Enemies
         }
         public Zombie(int value)
         {
+            health = value;
             if (value < 0)
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");

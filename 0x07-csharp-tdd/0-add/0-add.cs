722a1ab0390e5828eee5b215b2ab82ba1511cd9e
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-07-29 16:48:42 -0500

    add folder

---
+++ 0x07-csharp-tdd/0-add/0-add.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Add(int a, int b)
+        {
+            int c = a + b;
+            return c;
+        }
+    }
+}

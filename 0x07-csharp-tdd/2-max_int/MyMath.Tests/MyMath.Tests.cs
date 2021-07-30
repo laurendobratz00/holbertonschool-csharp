421262adcd486c64688fdaa82789f89f44fdc31f
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-07-30 14:15:49 -0500

    added tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -18,5 +18,42 @@ namespace MyMath.Tests
             int result = Operations.Max(myList);
             Assert.AreEqual(3, result);
         }
+                [Test]
+        public void MaxAtBeginning()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(3);
+            myList.Add(2);
+            myList.Add(1);
+            int result = Operations.Max(myList);
+            Assert.AreEqual(3, result);
+        }
+                [Test]
+        public void MaxInMiddle()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(1);
+            myList.Add(3);
+            myList.Add(2);
+            int result = Operations.Max(myList);
+            Assert.AreEqual(3, result);
+        }
+                [Test]
+        public void Negative()
+        {
+            List<int> myList = new List<int>();
+            myList.Add(-1);
+            myList.Add(-2);
+            myList.Add(-3);
+            int result = Operations.Max(myList);
+            Assert.AreEqual(-1, result);
+        }
+                [Test]
+        public void EmptyList()
+        {
+            List<int> myList = new List<int>();
+            int result = Operations.Max(myList);
+            Assert.AreEqual(0, result);
+        }
     }
 }

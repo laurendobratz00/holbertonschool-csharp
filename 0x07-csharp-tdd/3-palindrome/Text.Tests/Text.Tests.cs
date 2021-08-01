7f3fb932d4f2a3cdafb937f786b216032558d05b
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-01 16:51:38 -0500

    add folder

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    /// <summary>Test cases for 2-max_int</summary>
+    public class Tests
+    {
+        [Test]
+        public void MaxInt()
+        {
+            string s = "A man, a plan, a canal: Panama.";
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(true, result);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+            s = s.Replace(" ", String.Empty);
+            s = s.Replace(".", String.Empty);
+            s = s.Replace(":", String.Empty);
+            s = s.Replace(",", String.Empty);
+            if (s.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (s[0] != s[s.Length - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return IsPalindrome(s.Substring(1, s.Length - 2));
+                }
+            }
+        }
+    }
+}

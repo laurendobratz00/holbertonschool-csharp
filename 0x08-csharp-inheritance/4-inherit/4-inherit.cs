77c2120fd62ff3accc74f0da96b230cbe5df52ed
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-02 23:17:36 -0500

    add xml

--- 0x08-csharp-inheritance/4-inherit/4-inherit.cs
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 
+/// <summary>class Animal</summary>
 class Animal
 {
     // Empty class
 }
-
+/// <summary>class Dog : Animal</summary>
 class Dog : Animal
 {
     // Empty class

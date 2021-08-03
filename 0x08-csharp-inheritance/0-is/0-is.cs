e610209c9a8172420328364c7073922d8c5fd22b
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-02 21:31:32 -0500

    add folder

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+    class Obj
+    {
+        public static bool IsOfTypeInt(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (obj is int)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+

685c2606bdc83e3cff7c32cf190af470b782fcb6
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-02 22:23:59 -0500

    added comments

--- 0x08-csharp-inheritance/1-instance/1-instance.cs
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+/// <summary>Class obj</summary>
 class Obj
 {
+    /// <summary>IsInstanceOfArray</summary>
     public static bool IsInstanceOfArray(object obj)
     {
         return (typeof(Array).IsInstanceOfType(obj));

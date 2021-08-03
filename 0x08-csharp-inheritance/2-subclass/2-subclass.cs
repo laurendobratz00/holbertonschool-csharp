3d94dab30f9f636fe69591b852f4b5acb2b46268
Lauren Dobratz <2316@c02zx0umjv3x.hbtn.io>
2021-08-02 23:31:48 -0500

    add folder

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return true;
+        }
+        return false;
+    }
+}
+

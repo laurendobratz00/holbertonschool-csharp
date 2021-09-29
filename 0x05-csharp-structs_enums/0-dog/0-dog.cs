f493bbb00fb2e6b9473c3a4af9523d73fd87db9a
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-09-29 16:55:36 -0500

    Update 0-dog.cs

--- 0x05-csharp-structs_enums/0-dog/0-dog.cs
+++ 0x05-csharp-structs_enums/0-dog/0-dog.cs
@@ -1,12 +1,7 @@
-﻿using System;
+using System;
 
-namespace _0_dog
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+public enum Rating {
+  Good,
+  Great,
+  Excellent
 }

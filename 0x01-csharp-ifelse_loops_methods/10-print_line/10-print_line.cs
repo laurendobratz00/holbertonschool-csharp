959cecca15af172afa545f439e52c0acb58f1bed
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-02 15:42:14 -0500

    add folder

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine();
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write('_');
+            }
+            Console.WriteLine();
+        }
+    }
+}

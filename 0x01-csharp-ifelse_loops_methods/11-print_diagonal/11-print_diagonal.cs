cf31b521e19e01595ac6f0953bd454b7584fc44f
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-02 15:55:49 -0500

    add folder after a million tries

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine();
+        else
+        {
+            string j = "";
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(j + @"\");
+                j += " ";
+            }
+            Console.WriteLine();
+        }
+    }
+}

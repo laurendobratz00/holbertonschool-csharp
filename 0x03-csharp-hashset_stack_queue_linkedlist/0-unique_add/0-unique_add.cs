b40743bc6a4da58af74919ad54ad56fdab33344f
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-02 16:49:19 -0500

    add folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int i, sum = 0;
+
+        List<int> myList1;
+        myList1 = myList.Distinct().ToList();
+        for (i = 0; i < myList1.Count; i++)
+        {
+            sum += myList1[i];
+        }
+        return sum;
+    }
+}

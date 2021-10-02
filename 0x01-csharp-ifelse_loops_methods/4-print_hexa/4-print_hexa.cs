e50a485fcea773046d47c44d2d719be33ee0ddb2
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-02 14:32:17 -0500

    add folder

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 99; i++)
+            {
+                Console.WriteLine(i + " = " + "0x{0:x}", i);
+            }
+        }
+    }
+}

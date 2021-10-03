6d4266f110fc9d8b932557c5f8d42def46bbf664
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-03 07:38:30 -0500

    add folder

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    class Int
+    {
+        public static void divide(int a, int b)
+        {
+            int res = 0;
+            try
+            {
+                if (b != 0)
+                {
+                    res = a / b;
+                }
+            }
+            catch (DivideByZeroException)
+            {  
+                Console.WriteLine("Cannot divide by zero");
+            }
+            finally
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    Console.WriteLine(a + " / " + b + " = " + res);
+                }
+                else
+                {
+                    Console.WriteLine(a + " / " + b + " = " + res);
+                }
+            }
+        }
+    }
+

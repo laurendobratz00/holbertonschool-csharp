a42ea950eee90845bb3b657b38c5ddf873ae3685
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-03 08:35:56 -0500

    put the dumb for loop outside of the try/catch *rolls eyes*

--- 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ 0x04-csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-    class List
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
     {
-        public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+        List<int> newList = new List<int>();
+        int res = 0;
+        for (int i = 0; i < listLength; i++)
         {
-            List<int> newList = new List<int>();
-            int res = 0, i = 0;
             try
             {
-                for (i = 0; i < listLength; i++)
-                {
-                    res = list1[i] / list2[i];
-                    newList.Add(res);
-                }
+                res = list1[i] / list2[i];
+                newList.Add(res);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
             }
             catch (DivideByZeroException)
             {
                 newList.Add(0);
                 Console.WriteLine("Cannot divide by zero");
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                Console.WriteLine("Out of range");
-            }
-            return (newList);
         }
+        return (newList);
     }
+}

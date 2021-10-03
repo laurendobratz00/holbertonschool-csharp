b02414d23db517d95587e3b8ff30711481edaaa2
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-03 07:12:35 -0500

    add folder in 0x04

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            int j = 0;
+            try
+            {
+                for (int i = 0; i < myList.Count; i++)
+                {
+                    if (i < n)
+                    {
+                        Console.WriteLine(myList[i]);
+                        j++;
+                    }
+                }  
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return (j);
+        }
+    }

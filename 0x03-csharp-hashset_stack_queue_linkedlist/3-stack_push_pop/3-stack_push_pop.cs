1a3f3e1bbca13fdf30bc542a1740356a5b080108
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-04 14:45:03 -0500

    move stack contains line outside of the if statement

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -6,13 +6,13 @@ class MyStack
 {
     public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
     {
+        Console.WriteLine("Number of items: " + aStack.Count);
         if (aStack.Count == 0)
             Console.WriteLine("Stack is empty");
-        Console.WriteLine("Number of items: " + aStack.Count);
         Console.WriteLine("Top item: " + aStack.Peek());
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
         if (aStack.Contains(search))
         {
-            Console.WriteLine("Stack contains \"{0}\": {1}", search, aStack.Contains(search));
             int indexOfSearch = aStack.ToArray().ToList().IndexOf(search);
             while (aStack.Count > indexOfSearch - 1)
             {

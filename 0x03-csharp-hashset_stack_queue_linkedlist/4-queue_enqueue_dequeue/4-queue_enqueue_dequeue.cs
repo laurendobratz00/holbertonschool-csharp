eb7d2316a61c696bc4ddb7d9d97c1a4e63244196
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-04 15:16:52 -0500

    add folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aQueue.Count);
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine("First item: " + aQueue.Peek());
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, aQueue.Contains(search));
+        if (aQueue.Contains(search))
+        {
+            int index = aQueue.ToArray().ToList().IndexOf(search);
+            while (aQueue.Count > index - 1)
+            {
+                aQueue.Dequeue();
+            }
+        }
+        aQueue.Enqueue(newItem);
+        return (aQueue);
+    }
+}

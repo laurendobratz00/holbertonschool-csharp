57014a90fb129f0c9a9c935e6ef0ffb2785be9e2
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-04 14:34:58 -0500

    add folder

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        if (aStack.Count == 0)
+            Console.WriteLine("Stack is empty");
+        Console.WriteLine("Number of items: " + aStack.Count);
+        Console.WriteLine("Top item: " + aStack.Peek());
+        if (aStack.Contains(search))
+            Console.WriteLine("Stack contains " + search + ": True");
+        else
+            Console.WriteLine("Stack contains " + search + ": False");
+        int indexOfSearch = aStack.ToArray().ToList().IndexOf(search);
+        while (aStack.Count > indexOfSearch - 1)
+            aStack.Pop();
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
+
--- 0x08-csharp-inheritance/7-shape/7-shape.cs
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
-namespace _7_shape
+///<summary> Shape </summary>
+class Shape
 {
-    class Program
+    ///<summary> Area </summary>
+    public virtual int Area()
     {
-        static void Main(string[] args)
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+///<summary> Rectangle </summary>
+class Rectangle : Shape
+{
+    ///<summary> Attributes </summary>
+    private int _width;
+    private int _height;
+    ///<summary> Width </summary>
+    public int Width
+    {
+        get
         {
-            Console.WriteLine("Hello World!");
+            return _width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                _width = value;
+            }
+        }
+    }
+
+    ///<summary> Height </summary>
+    public int Height
+    {
+        get
+        {
+            return _height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                _height = value;
+            }
         }
     }
 }

95381e8871d7c05e9c93ccf75da67b30a5b136fe
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-05 19:26:12 -0500

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> CreatePrint(int size)
+        {
+            List<int> newList = new List<int>();
+            if (size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return (null);
+            }
+            else
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    newList.Add(i);
+                    Console.Write(i);
+                    if (i == size - 1)
+                    {
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+            return (newList);
+        }
+    }
+

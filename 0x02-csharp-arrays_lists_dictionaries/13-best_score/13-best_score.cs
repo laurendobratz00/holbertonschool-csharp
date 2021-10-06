46a7bb664f37ce5984ddb2c8e7bbe2f187f751b0
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-06 00:01:53 -0500

    changed null statement again

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -6,7 +6,7 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
-        if (myList.Count == null)
+        if (myList.Count == 0)
             return ("None");
         string maxKey = myList.Aggregate((x, y) => x.Value > y.Value ? x : y).Key;
         return (maxKey);

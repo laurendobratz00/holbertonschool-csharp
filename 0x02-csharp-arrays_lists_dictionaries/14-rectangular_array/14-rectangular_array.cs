2ef36c112be2cd28418dcf69826e9d59039d49a7
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-06 00:47:53 -0500

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5,5] {{0,0,0,0,0}, {0,0,0,0,0}, {0,0,1,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
+            int i, j;
+
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (j == (a.GetLength(1)) - 1)
+                    {
+                        Console.Write(string.Format("{0}", a[i, j]));
+                    }
+                    else
+                    {
+                        Console.Write(string.Format("{0} ", a[i, j]));
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

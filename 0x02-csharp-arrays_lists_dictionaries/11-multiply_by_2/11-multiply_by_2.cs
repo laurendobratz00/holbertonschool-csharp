aa1a1d72b70b98a70721b2e28939819fa1ee12d1
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-05 22:12:08 -0500

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Dictionary
+    {
+        public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+        {
+            Dictionary<string, int> newDict = new Dictionary<string, int>();
+            foreach (var val in myDict)
+            {
+                newDict.Add(val.Key, val.Value * 2);
+            }
+            return (newDict);
+        }
+    }
+

9d7cc66b0adb45a7c6666858ff68341d56a35e89
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-05 23:51:01 -0500

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Keys == null)
+            return ("None");
+        string maxKey = myList.Keys.Max();
+        return (maxKey);
+    }
+}

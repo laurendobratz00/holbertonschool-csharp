84933a9700904abb7926016d277f5a5f25e8f7fd
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-05 22:40:24 -0500

    add folder

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+    class Dictionary
+    {
+        public static void PrintSorted(Dictionary<string, string> myDict)
+        {
+            ///var list = myDict.Keys.ToList();
+            ///list.Sort();
+            var myList = myDict.Keys.ToList();
+            myList.Sort();
+            foreach (var key in myList)
+            {
+                Console.WriteLine("{0}: {1}", key, myDict[key]);
+            }
+        }
+    }

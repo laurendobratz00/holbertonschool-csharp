0ad7ea66f34a973fb32a86ea00d86711f234b0a8
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-08 02:05:17 -0500

    add folder

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,6 +5,15 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-
+        LinkedListNode<int> node = myLList.First;
+        for (int i = 0; i <= index && node != null; i++)
+        {
+            if (i != index)
+            {
+                node = node.Next;
+                continue;
+            }
+            myLList.Remove(node);
+        }
     }
 }

890b8338d409cedab9c9023eda01d942370101ae
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-25 21:13:04 -0500

    add folder

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+///<summary> returns the Queue's type </summary>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+    ///<summary> returns the Queue's type </summary>
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+        public Node(T i)
+        {
+            value = i;
+        }
+    }
+    public void Enqueue(T value)
+    {
+        Node newnode = new Node(value);
+        if (head == null)
+        {
+            head = newnode;
+        }
+        else if (tail == null)
+        {
+            tail = newnode;
+        }
+        else
+        {
+            tail.next = newnode;
+            tail = newnode;
+        }
+        count++;
+    }
+    public int Count()
+    {
+        return count;
+    }
+}

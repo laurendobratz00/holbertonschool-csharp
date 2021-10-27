fc69d7025293774e65ebbd9248d9b47561b638dd
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-26 19:54:20 -0500

    add folder

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,51 @@
+﻿using System;
+///<summary> abstract class Base </summary>
+
+public abstract class Base
+{
+    ///<summary> abstract class Base </summary>
+    public string name = "";
+
+    public override string ToString()
+    {
+        Type tp = name.GetType();
+        return (name + " is a " + this.GetType());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+interface IBreakable
+{
+    int durability
+    {
+        get;
+        set;
+    }
+    void Break();
+}
+interface ICollectable
+{
+    bool isCollected
+    {
+        get;
+        set;
+    }
+    void Collect();
+}
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; } = 1;
+    public bool isCollected { get; set; } = false;
+    public void Interact()
+    {
+    }
+    public void Break()
+    {
+    }
+    public void Collect()
+    {
+    }
+}

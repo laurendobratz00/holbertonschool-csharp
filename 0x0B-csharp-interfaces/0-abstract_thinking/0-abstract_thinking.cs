b1cbf77d50a2d3b1ae9c867dfe00240f805b64a5
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-26 19:19:51 -0500

    add folder

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+///<summary> abstract class Base </summary>
+
+abstract class Base
+{
+    ///<summary> abstract class Base </summary>
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        Type tp = name.GetType();
+        return (name + " is a " + this.GetType());
+    }
+}

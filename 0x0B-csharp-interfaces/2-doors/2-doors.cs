72d5a552bd813ccaf9f0a8d94789694f1e746395
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-26 20:19:03 -0500

    add folder

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+///<summary> abstract class Base </summary>
+public abstract class Base
+{
+    ///<summary> name </summary>
+    public string name { get; set; }
+
+    ///<summary> override tostring method </summary>
+    public override string ToString()
+    {
+        Type tp = name.GetType();
+        return (name + " is a " + this.GetType());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+interface IBreakable
+{
+    int durability
+    {
+        get;
+        set;
+    }
+    void Break();
+}
+interface ICollectable
+{
+    bool isCollected
+    {
+        get;
+        set;
+    }
+    void Collect();
+}
+///<summary> class Door </summary>
+public class Door : Base, IInteractive
+{
+    ///<summary> Door constructor </summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+    ///<summary> Interact method </summary>
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + this.name + ". It's locked.");
+    }
+}

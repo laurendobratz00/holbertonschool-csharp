cb7985bc2bb73a937e1733b1db30de5beeca3d77
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-10-28 17:54:04 -0500

    add folder

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+///<summary> class Player </summary>
+public class Player
+{
+    private string name { get; set; }
+    private float maxHp { get; set; }
+    private float hp { get; set; }
+
+    ///<summary> Player constructor </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    ///<summary> Prints the player's health status </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+}

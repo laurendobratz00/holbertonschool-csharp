7b69ac6178590b6920efac147d0d1faa600d6c27
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2021-11-04 22:29:52 -0500

    add task 1

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryLibrary
+{
+    ///<summary> class Inventory </summary>
+    class Inventory: BaseClass
+    {
+        ///<summary> user_id </summary>
+        public int user_id;
+        ///<summary> item_id </summary>
+        public int item_id;
+        ///<summary> quantity </summary>
+        public uint quantity = 1;
+    }
+}
---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryLibrary
+{
+    ///<summary> class User </summary>
+    class User: BaseClass
+    {
+        ///<summary> name </summary>
+        public string name;
+    }
+}

c845162ec8a8350fc89704d06194c6f955108a7d
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2022-02-03 23:38:17 -0600

    add comments

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -2,8 +2,10 @@
 
 namespace _3_writeline
 {
+    /// <summary> class Program </summary>
     class Program
     {
+        /// <summary> script that initializes a new C# project inside a folder </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("\"Programming is like building a multilingual puzzle");

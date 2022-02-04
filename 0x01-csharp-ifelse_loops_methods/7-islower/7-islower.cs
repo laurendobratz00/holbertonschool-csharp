1c101c03ec840139ada49bb4439acfe749fffc2d
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2022-02-04 00:00:36 -0600

    add comments

--- 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 
-
+    /// <summary> class Character </summary>
     class Character
     {
+        /// <summary> return true if lowercase letters </summary>
         public static bool IsLower(char c)
         {
             if (c >= 'a' && c <= 'z')

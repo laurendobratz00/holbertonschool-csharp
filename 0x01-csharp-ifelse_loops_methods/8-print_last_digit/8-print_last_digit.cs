44fb815ca4b6823b59f7b014e9ec767f1c82e44b
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2022-02-04 00:01:17 -0600

    add comments

--- 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+/// <summary> class Number </summary>
 class Number
 {
+    /// <summary> print the last digit of a number </summary>
     public static int PrintLastDigit(int number)
     {
         int newnum = number % 10;

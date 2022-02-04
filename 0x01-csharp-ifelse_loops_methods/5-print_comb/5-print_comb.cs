ddb4f15540f29c5699cfe43a7f50c34c11cf1ba7
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2022-02-03 23:58:22 -0600

    add comments

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -2,8 +2,10 @@
 
 namespace _5_print_comb
 {
+    /// <summary> class Program </summary>
     class Program
     {
+        /// <summary> print comb </summary>
         static void Main(string[] args)
         {
             for (int i = 0; i < 100; i++)

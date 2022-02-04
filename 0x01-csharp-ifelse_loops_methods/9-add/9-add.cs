d4d51bec3872e909cdec4702f4d6db31706f6b42
laurendobratz00 <61027706+laurendobratz00@users.noreply.github.com>
2022-02-04 00:02:52 -0600

    add comments

--- 0x01-csharp-ifelse_loops_methods/9-add/9-add.cs
+++ 0x01-csharp-ifelse_loops_methods/9-add/9-add.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+/// <summary> class Number </summary>
 class Number
 {
+    /// <summary> add two numbers </summary>
     public static int Add(int a, int b)
     {
         int c = a + b;
